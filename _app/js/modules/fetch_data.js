5bb8b29d4dbf48a7fc5940186051b96f66992360
sergei <ravinski.sergei@gmail.com>
2023-02-10 17:15:46 +0100

    Added elements from API

--- _app/js/modules/fetch_data.js
+++ _app/js/modules/fetch_data.js
@@ -6,40 +6,113 @@ export const fetchData = async () => {
     //console.log(response)
     const events = [result];
 
-
-    const divElement = document.getElementById('events');
-
-    //for (const event of events) {
-    //    renderList(event);
-    //    console.log(event._embedded.events[3]) 
-    //}
     events.forEach(event => {
             renderList(event);
-                console.log(event) 
-    })
+                //console.log(event) 
+    });
 
     function renderList(event) {
 
-        const divForImages = document.createElement('img');
-        divForImages.setAttribute('src', `${event._embedded.events[0].images[0]}`);
-        divElement.appendChild(divForImages);
+        const sectionEvents = document.querySelector('.main__events');
 
-        const liForEvents = document.createElement('li');
-        liForEvents.textContent = (`Event: ${event._embedded.events[6].name}, genre: ${event._embedded.events[6].classifications[0].genre.name}, location: (${event._embedded.events[6].dates.timezone}), promoter: ${event._embedded.events[6].promoter.name}`);
-        divElement.appendChild(liForEvents);
+        const newEvent = document.createElement('div')
+        newEvent.innerHTML = `
+            <div class="main__images-filter-aalesund main__images-filter-all">
+                <button></button>
+                <a href="#">
+                    <img src="${event._embedded.events[0].images[0].url}" alt="">
+                </a>
+                <span>
+                    <h5>${event._embedded.events[0].name}</h5>
+                    <h6>Pris fra ${event._embedded.events[0].priceRanges[0].min} nok</h6>
+                    <h6>${event._embedded.events[0].dates.start.localDate}</h6>
+                </span>
+            </div>
+            <div class="main__images-filter-aalesund main__images-filter-all">
+                <button></button>
+                <a href="#">
+                    <img src="${event._embedded.events[3].images[3].url}" alt="">
+                </a>
+                <span>
+                    <h5>${event._embedded.events[3].name}</h5>
+                    <h6>Pris fra ${event._embedded.events[3].priceRanges[0].min} nok</h6>
+                    <h6>${event._embedded.events[3].dates.start.localDate}</h6>
+                </span>
+            </div>
 
-        const liForEvents1 = document.createElement('li');
-        liForEvents1.textContent = (`Event: ${event._embedded.events[9].name}, genre: ${event._embedded.events[9].classifications[0].genre.name}, location: (${event._embedded.events[9].dates.timezone}), promoter: ${event._embedded.events[9].promoter.name}`);
-        divElement.appendChild(liForEvents1);
+            <div class="main__images-filter-aalesund main__images-filter-all">
+                <button></button>
+                <a href="#">
+                    <img src="${event._embedded.events[9].images[9].url}" alt="">
+                </a>
+                <span>
+                    <h5>${event._embedded.events[9].name}</h5>
+                    <h6>Pris fra ${event._embedded.events[9].priceRanges[0].min} nok</h6>
+                    <h6>${event._embedded.events[9].dates.start.localDate}</h6>
+                </span>
+            </div>
 
-        const liForEvents2 = document.createElement('li');
-        liForEvents2.textContent = (`Event: ${event._embedded.events[17].name}, genre: ${event._embedded.events[17].classifications[0].genre.name}, location: (${event._embedded.events[17].dates.timezone}), promoter: ${event._embedded.events[17].promoter.name}`);
-        divElement.appendChild(liForEvents2);
+            <div class="main__images-filter-aalesund main__images-filter-all">
+                <button></button>
+                <a href="#">
+                    <img src="${event._embedded.events[8].images[8].url}" alt="">
+                </a>
+                <span>
+                    <h5>${event._embedded.events[16].name}</h5>
+                    <h6>Pris fra ${event._embedded.events[16].priceRanges[0].min} nok</h6>
+                    <h6>${event._embedded.events[16].dates.start.localDate}</h6>
+                </span>
+            </div>
 
-        const liForEvents3 = document.createElement('li');
-        liForEvents3.textContent = (`Event: ${event._embedded.events[0].name}, genre: ${event._embedded.events[0].classifications[0].genre.name}, location: (${event._embedded.events[0].dates.timezone}), promoter: ${event._embedded.events[0].promoter.name}`);
-        divElement.appendChild(liForEvents3);
+            <div class="main__images-filter-aalesund main__images-filter-all">
+                <button></button>
+                <a href="#">
+                    <img src="${event._embedded.events[2].images[2].url}" alt="">
+                </a>
+                <span>
+                    <h5>${event._embedded.events[2].name}</h5>
+                    <h6>Pris fra ${event._embedded.events[2].priceRanges[0].min} nok</h6>
+                    <h6>${event._embedded.events[2].dates.start.localDate}</h6>
+                </span>
+            </div>
+
+            <div class="main__images-filter-aalesund main__images-filter-all">
+            <button></button>
+            <a href="#">
+                <img src="${event._embedded.events[5].images[5].url}" alt="">
+            </a>
+            <span>
+                <h5>${event._embedded.events[5].name}</h5>
+                <h6>Pris fra ${event._embedded.events[5].priceRanges[0].min} nok</h6>
+                <h6>${event._embedded.events[5].dates.start.localDate}</h6>
+            </span>
+        </div>
+        `;
+        sectionEvents.appendChild(newEvent);
+
+        //sectionEvents.textContent = (`Event: ${event._embedded.events[6].name}, genre: ${event._embedded.events[6].classifications[0].genre.name}, location: (${event._embedded.events[6].dates.timezone}), promoter: ${event._embedded.events[6].promoter.name}`);
+        //divElement.appendChild(sectionEvents);
+
+
+
+        //const divForImages = document.createElement('img');
+        //divForImages.setAttribute('src', `${event._embedded.events[0].images[0]}`);
+        //divElement.appendChild(divForImages);
+
+        //const liForEvents1 = document.createElement('li');
+        //liForEvents1.textContent = (`Event: ${event._embedded.events[9].name}, genre: ${event._embedded.events[9].classifications[0].genre.name}, location: (${event._embedded.events[9].dates.timezone}), promoter: ${event._embedded.events[9].promoter.name}`);
+        //divElement.appendChild(liForEvents1);
+
+        //const liForEvents2 = document.createElement('li');
+        //liForEvents2.textContent = (`Event: ${event._embedded.events[17].name}, genre: ${event._embedded.events[17].classifications[0].genre.name}, location: (${event._embedded.events[17].dates.timezone}), promoter: ${event._embedded.events[17].promoter.name}`);
+        //divElement.appendChild(liForEvents2);
+
+        //const liForEvents3 = document.createElement('li');
+        //liForEvents3.textContent = (`Event: ${event._embedded.events[0].name}, genre: ${event._embedded.events[0].classifications[0].genre.name}, location: (${event._embedded.events[0].dates.timezone}), promoter: ${event._embedded.events[0].promoter.name}`);
+        //divElement.appendChild(liForEvents3);
+
     }
+    //renderList()
 
 
 
