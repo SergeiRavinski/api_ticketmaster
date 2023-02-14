22c974e06576bc6a8443da6173a77d96a8f122ae
sergei <ravinski.sergei@gmail.com>
2023-02-14 16:07:51 +0100

    Created filter function

--- _app/js/modules/fetch_data.js
+++ _app/js/modules/fetch_data.js
@@ -6,16 +6,18 @@ export const fetchData = async () => {
     const result = await response.json();
     //console.log(result);
     showEvents(result);
+    showEventsOslo(result);
+    showEventsBergen(result);
 }
 
 function showEvents(data) {
 
-    const sectionEvents = document.querySelector('.main__events');
+    const sectionEvents = document.querySelector('.main__events-events');
     data._embedded.events.forEach((event) => {
     
         const newEvent = document.createElement('div');     
         newEvent.innerHTML = `
-            <div class="main__images-filter-aalesund main__images-filter-all">
+            <div>
                 <button></button>
                 <a href="${event.url}">
                     <img src="${event.images[3].url}" alt="">
@@ -31,6 +33,60 @@ function showEvents(data) {
         sectionEvents.appendChild(newEvent);      
     });
 }        
+
+function showEventsOslo(data) {
+
+    const sectionEventsOslo = document.querySelector('.main__events-oslo');
+    data._embedded.events.filter(event => {
+
+        if (event._embedded.venues[0].city.name === 'Oslo') {
+    
+            const newEventOslo = document.createElement('div');     
+            newEventOslo.innerHTML = `
+                <div>
+                    <button></button>
+                    <a href="${event.url}">
+                        <img src="${event.images[3].url}" alt="">
+                    </a>
+                    <span>
+                        <h5>${event.name}</h5>
+                        <h6>${event._embedded.venues[0].city.name}</h6>
+                        <h6>Pris fra ${event.priceRanges[0].min} ${event.priceRanges[0].currency}</h6>
+                        <h6>${event.dates.start.localDate}</h6>
+                    </span>
+                </div>
+            `;
+            sectionEventsOslo.appendChild(newEventOslo);  
+        }    
+    });
+}   
+
+function showEventsBergen(data) {
+
+    const sectionEventsBergen = document.querySelector('.main__events-bergen');
+    data._embedded.events.filter(event => {
+
+        if (event._embedded.venues[0].city.name === 'Bergen') {
+    
+            const newEventBergen = document.createElement('div');     
+            newEventBergen.innerHTML = `
+                <div>
+                    <button></button>
+                    <a href="${event.url}">
+                        <img src="${event.images[3].url}" alt="">
+                    </a>
+                    <span>
+                        <h5>${event.name}</h5>
+                        <h6>${event._embedded.venues[0].city.name}</h6>
+                        <h6>Pris fra ${event.priceRanges[0].min} ${event.priceRanges[0].currency}</h6>
+                        <h6>${event.dates.start.localDate}</h6>
+                    </span>
+                </div>
+            `;
+            sectionEventsBergen.appendChild(newEventBergen);  
+        }    
+    });
+}   
     
 
 
