8c54e04a30ab21e908bba4a075d7d972b255b082
sergei <ravinski.sergei@gmail.com>
2023-02-16 17:11:53 +0100

    Imported js data

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,12 +1,16 @@
-import fetchData from './modules/fetch_data.js';
-//import RenderHTML from './modules/renderHTML.js'
-import NavigationMobile from './modules/navigation.js';
-import Scroll_up from './modules/scroll_up.js';
-import Animate_on_scroll from './modules/animate_on_scroll.js';
+import { fetchData } from './modules/fetch_data.js';
+import renderHTMLEvents from './modules/renderHTMLEvents.js'
+import renderHTMLFindMusic from './modules/renderHTMLFindMusic.js'
+import navigationMobile from './modules/navigation.js';
+import scrollUp from './modules/scroll_up.js';
+import animateOnScroll from './modules/animate_on_scroll.js';
 
 fetchData();
-//RenderHTML(await FetchData());
-NavigationMobile();
-Scroll_up();
-Animate_on_scroll();
+renderHTMLEvents(await fetchData());
+renderHTMLFindMusic(await fetchData());
+navigationMobile();
+scrollUp();
+animateOnScroll();
+
+
 
