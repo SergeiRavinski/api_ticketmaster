6163fe3d918a675bc96dc42880e16e93d3501f9c
sergei <ravinski.sergei@gmail.com>
2023-02-16 11:06:41 +0100

    Added animation on hover

--- _app/js/modules/fetch_data.js
+++ _app/js/modules/fetch_data.js
@@ -1,6 +1,6 @@
 export default async function fetchData() {
     const apiKey = 'WyccpB6oo7rfmkGMeApP2BriNGE68Y1j'
-    const currentEvents = 20;
+    const currentEvents = 10;
     const rootUrl = `https://app.ticketmaster.com/discovery/v2`;
     const endpoint = `${rootUrl}/events?&apikey=${apiKey}&size=${currentEvents}&locale=no`;
     const response = await fetch(endpoint);
@@ -36,59 +36,59 @@ function showEvents(data) {
     });
 }        
 
-function showEventsOslo(data) {
+//function showEventsOslo(data) {
 
-    const sectionEventsOslo = document.querySelector('.main__events-oslo');
-    data._embedded.events.find(event => {
+//    const sectionEventsOslo = document.querySelector('.main__events-oslo');
+//    data._embedded.events.find(event => {
 
-        if (event._embedded.venues[0].city.name === 'Bergen') {
+//        if (event._embedded.venues[0].city.name === 'Bergen') {
     
-            const newEventOslo = document.createElement('div');     
-            newEventOslo.innerHTML = `
-                <div>
-                    <button></button>
-                    <a href="${event.url}">
-                        <img src="${event.images[3].url}" alt="">
-                    </a>
-                    <span>
-                        <h5>${event.name}</h5>
-                        <h6>${event._embedded.venues[0].city.name}</h6>
-                        <h6>Pris fra ${event.priceRanges[0].min} ${event.priceRanges[0].currency}</h6>
-                        <h6>${event.dates.start.localDate}</h6>
-                    </span>
-                </div>
-            `;
-            sectionEventsOslo.appendChild(newEventOslo);  
-        }    
-    });
-}   
+//            const newEventOslo = document.createElement('div');     
+//            newEventOslo.innerHTML = `
+//                <div>
+//                    <button></button>
+//                    <a href="${event.url}">
+//                        <img src="${event.images[3].url}" alt="">
+//                    </a>
+//                    <span>
+//                        <h5>${event.name}</h5>
+//                        <h6>${event._embedded.venues[0].city.name}</h6>
+//                        <h6>Pris fra ${event.priceRanges[0].min} ${event.priceRanges[0].currency}</h6>
+//                        <h6>${event.dates.start.localDate}</h6>
+//                    </span>
+//                </div>
+//            `;
+//            sectionEventsOslo.appendChild(newEventOslo);  
+//        }    
+//    });
+//}   
 
-function showEventsBergen(data) {
+//function showEventsBergen(data) {
 
-    const sectionEventsBergen = document.querySelector('.main__events-bergen');
-    data._embedded.events.find(event => {
+//    const sectionEventsBergen = document.querySelector('.main__events-bergen');
+//    data._embedded.events.find(event => {
 
-        if (event._embedded.venues[0].city.name === 'Oslo') {
+//        if (event._embedded.venues[0].city.name === 'Oslo') {
     
-            const newEventBergen = document.createElement('div');     
-            newEventBergen.innerHTML = `
-                <div>
-                    <button></button>
-                    <a href="${event.url}">
-                        <img src="${event.images[3].url}" alt="">
-                    </a>
-                    <span>
-                        <h5>${event.name}</h5>
-                        <h6>${event._embedded.venues[0].city.name}</h6>
-                        <h6>Pris fra ${event.priceRanges[0].min} ${event.priceRanges[0].currency}</h6>
-                        <h6>${event.dates.start.localDate}</h6>
-                    </span>
-                </div>
-            `;
-            sectionEventsBergen.appendChild(newEventBergen);  
-        }    
-    });
-}   
+//            const newEventBergen = document.createElement('div');     
+//            newEventBergen.innerHTML = `
+//                <div>
+//                    <button></button>
+//                    <a href="${event.url}">
+//                        <img src="${event.images[3].url}" alt="">
+//                    </a>
+//                    <span>
+//                        <h5>${event.name}</h5>
+//                        <h6>${event._embedded.venues[0].city.name}</h6>
+//                        <h6>Pris fra ${event.priceRanges[0].min} ${event.priceRanges[0].currency}</h6>
+//                        <h6>${event.dates.start.localDate}</h6>
+//                    </span>
+//                </div>
+//            `;
+//            sectionEventsBergen.appendChild(newEventBergen);  
+//        }    
+//    });
+//}   
     
 
 
