e5b36f06c9305a7a9aa29efe45f5eb926e80498e
sergei <ravinski.sergei@gmail.com>
2023-02-17 10:15:59 +0100

    Changed endpoint

--- _app/js/modules/fetch_data.js
+++ _app/js/modules/fetch_data.js
@@ -2,7 +2,7 @@ export const fetchData = async () => {
     const apiKey = 'WyccpB6oo7rfmkGMeApP2BriNGE68Y1j'
     const currentEvents = 10;
     const rootUrl = `https://app.ticketmaster.com/discovery/v2`;
-    const endpoint = `${rootUrl}/events?&apikey=${apiKey}&size=${currentEvents}&countryCode=no`;
+    const endpoint = `${rootUrl}/events?&apikey=${apiKey}&size=${currentEvents}&countryCode=no&sort=name,date,asc`;
     const response = await fetch(endpoint);
     const result = await response.json();
     const { events } = result._embedded;
