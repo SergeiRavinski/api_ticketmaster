41d5caa9b3c88fe816c2129f9a0ec48aab7f469a
sergei <ravinski.sergei@gmail.com>
2023-03-03 20:14:01 +0100

    Rewrote innerHTML

--- _app/js/modules/renderHTMLEvents.js
+++ _app/js/modules/renderHTMLEvents.js
@@ -1,6 +1,5 @@
 export default function renderHTMLEvents(data, type) {
 
-
 	const modalWindow = document.querySelector('.main__modal-window');
 
 	let sectionEvents	= '';
@@ -24,7 +23,7 @@ export default function renderHTMLEvents(data, type) {
 		const newHSixFirst = document.createElement('h6');
 		const newHSixSecond = document.createElement('h6');
 	
-		newImg.src = `${event.images[3].url}`;
+		newImg.src = `${event.images[2].url}`;
 		//newLink.href = `${event.url}`;
 		newHFive.textContent = `${event.name}`;
 		newHSixFirst.textContent = `${event._embedded.venues[0].city.name}`;
@@ -53,43 +52,133 @@ export default function renderHTMLEvents(data, type) {
 
 	function openModalWindow(event) {
 
+		
+
 		if(modalWindow) {
 			modalWindow.classList.add('main__modal-window--visible');
 		}		
+		//console.log(event)
 
-		modalWindow.innerHTML = `
-			<div class="main__modal-window-card">
-				<div class="main__modal-window-card-first-section">
-					<div class="main__modal-window-card-first-section-image">
-						<img src="${event.images[3].url}" alt="image">
-					</div>
-					<div class="main__modal-window-card-first-section-info">
-						<h5>${event.name}</h5>
-						<h6>Date: ${event.dates.start.localDate}</h6>
-						<h6>Price: ${event.priceRanges[0].min} NOK - ${event.priceRanges[0].max} NOK</h6>
-					</div>
-				</div>
-				<button id="main__modal-window-card-button-close">
-					<img src="./_app/assets/icons/close_modal_window.svg" alt="">
-				</button>
-				<div class="main__modal-window-card-second-section">
-					<span class="main__modal-window-card-second-section-address">
-						<h5>Venue: ${event._embedded.venues[0].name}</h5>
-						<h6>Address: ${event._embedded.venues[0].address.line1}</h6>
-					</span>
-					<div class="main__modal-window-card-second-section-image">
-						<img src="./_app/assets/images/map.jpeg" alt="map">
-					</div>	
-				</div>
-				<button class="main__modal-window-card-button-buy">Buy tickets</button>
-			</div>
-		`
-		const buttonCloseModalWindow = document.getElementById('main__modal-window-card-button-close');
-		buttonCloseModalWindow.addEventListener('click', handleCloseModalWindow);
+		const newDivModalWindow = document.createElement('div');
+		newDivModalWindow.className = 'main__modal-window-card';
+
+		const newDivModalWindowFirstSection = document.createElement('div');
+		newDivModalWindowFirstSection.className = 'main__modal-window-card-first-section';
+
+		const newDivModalWindowFirstSectionImage = document.createElement('div');
+		newDivModalWindowFirstSectionImage.className = 'main__modal-window-card-first-section-image';
+
+		const newDivModalWindowFirstSectionImageElement = document.createElement('img');
+		newDivModalWindowFirstSectionImageElement.src = `${event.images[2].url}`;
+		const newDivModalWindowFirstSectionInfo = document.createElement('span');
+		newDivModalWindowFirstSectionInfo.className = 'main__modal-window-card-first-section-info';
+
+		const newDivModalWindowFirstSectionInfoHFive = document.createElement('h5');
+		newDivModalWindowFirstSectionInfoHFive.innerText = `${event.name}`;
+		const newDivModalWindowFirstSectionInfoHSixFirst = document.createElement('h6');
+		newDivModalWindowFirstSectionInfoHSixFirst.innerText = `Date: ${event.dates.start.localDate}`;
+		const newDivModalWindowFirstSectionInfoHSixSecond = document.createElement('h6');
+		newDivModalWindowFirstSectionInfoHSixSecond.innerText = `Price: ${event.priceRanges[0].min} - ${event.priceRanges[0].max} NOK`;
+		const newDivModalWindowFirstSectionButtonClose = document.createElement('button');
+		newDivModalWindowFirstSectionButtonClose.className = 'main__modal-window-card-button-close';
+
+		const newDivModalWindowFirstSectionButtonCloseIcon = document.createElement('img');
+		newDivModalWindowFirstSectionButtonCloseIcon.src = './_app/assets/icons/close_modal_window.svg';
+		const newDivModalWindowSecondSection = document.createElement('div');
+		newDivModalWindowSecondSection.className = 'main__modal-window-card-second-section';
+
+		const newDivModalWindowSecondSectionSpan = document.createElement('span');
+		newDivModalWindowSecondSectionSpan.className = 'main__modal-window-card-second-section-address';
+
+		const newDivModalWindowSecondSectionSpanHFive = document.createElement('h5');
+		newDivModalWindowSecondSectionSpanHFive.innerText = `Venue: ${event._embedded.venues[0].name}`;
+		const newDivModalWindowSecondSectionSpanHSix = document.createElement('h6');
+		newDivModalWindowSecondSectionSpanHSix.innerText = `Address: ${event._embedded.venues[0].address.line1}`;
+		const newDivModalWindowSecondSectionImage = document.createElement('div');
+		newDivModalWindowSecondSectionImage.className = 'main__modal-window-card-second-section-image';
+
+		const newDivModalWindowSecondSectionImageElement = document.createElement('img');
+		newDivModalWindowSecondSectionImageElement.src = './_app/assets/images/map.jpeg';
+		const newDivModalWindowSecondSectionLinkBuy = document.createElement('a');
+		newDivModalWindowSecondSectionLinkBuy.href = `${event.url}`;
+		newDivModalWindowSecondSectionLinkBuy.className = 'main__modal-window-card-button-buy';
+
+		const newDivModalWindowSecondSectionButton = document.createElement('button');
+		newDivModalWindowSecondSectionButton.innerText = 'Buy tickets';
+
+		newDivModalWindow.append(
+			newDivModalWindowFirstSection,
+			newDivModalWindowFirstSectionButtonClose,
+			newDivModalWindowSecondSection,
+			newDivModalWindowSecondSectionLinkBuy
+		)
+
+		newDivModalWindowFirstSection.append(
+			newDivModalWindowFirstSectionImage,
+			newDivModalWindowFirstSectionInfo,
+		)
+
+		newDivModalWindowFirstSectionImage.appendChild(newDivModalWindowFirstSectionImageElement);
+		newDivModalWindowFirstSectionInfo.append(
+			newDivModalWindowFirstSectionInfoHFive,
+			newDivModalWindowFirstSectionInfoHSixFirst,
+			newDivModalWindowFirstSectionInfoHSixSecond
+		)
+
+		newDivModalWindowFirstSectionButtonClose.appendChild(newDivModalWindowFirstSectionButtonCloseIcon);
+
+		newDivModalWindowSecondSection.append(
+			newDivModalWindowSecondSectionSpan,
+			newDivModalWindowSecondSectionImage
+		)
+
+		newDivModalWindowSecondSectionSpan.append(
+			newDivModalWindowSecondSectionSpanHFive,
+			newDivModalWindowSecondSectionSpanHSix
+		)
+
+		newDivModalWindowSecondSectionImage.appendChild(newDivModalWindowSecondSectionImageElement);
+		newDivModalWindowSecondSectionLinkBuy.appendChild(newDivModalWindowSecondSectionButton);	
+
+		modalWindow.appendChild(newDivModalWindow);
+		
+
+		//modalWindow.innerHTML = `
+		//	<div class="main__modal-window-card">
+		//		<div class="main__modal-window-card-first-section">
+		//			<div class="main__modal-window-card-first-section-image">
+		//				<img src="${event.images[2].url}" alt="image">
+		//			</div>
+		//			<span class="main__modal-window-card-first-section-info">
+		//				<h5>${event.name}</h5>
+		//				<h6>Date: ${event.dates.start.localDate}</h6>
+		//				<h6>Price: ${event.priceRanges[0].min} NOK - ${event.priceRanges[0].max} NOK</h6>
+		//			</span>
+		//		</div>
+		//		<button class="main__modal-window-card-button-close">
+		//			<img src="./_app/assets/icons/close_modal_window.svg" alt="">
+		//		</button>
+
+		//		<div class="main__modal-window-card-second-section">
+		//			<span class="main__modal-window-card-second-section-address">
+		//				<h5>Venue: ${event._embedded.venues[0].name}</h5>
+		//				<h6>Address: ${event._embedded.venues[0].address.line1}</h6>
+		//			</span>
+		//			<div class="main__modal-window-card-second-section-image">
+		//				<img src="./_app/assets/images/map.jpeg" alt="map">
+		//			</div>	
+		//		</div>
+		//		<a class="main__modal-window-card-button-buy" href="${event.url}">
+		//			<button>Buy tickets</button>
+		//		</a>
+		//	</div>
+		//`
+		newDivModalWindowFirstSectionButtonClose.addEventListener('click', handleCloseModalWindow);
 	}
 
 	function handleCloseModalWindow() {
 		modalWindow.classList.remove('main__modal-window--visible');
+
 	}
 
 	window.addEventListener('keydown', handleCloseModalWindowEscape);
